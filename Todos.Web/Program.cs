8b9293b59168199d57b00f0603170de9440f9be0
Pavel Henkin <pavel.henkin@gmail.com>
2018-10-22 10:10:38 -0700

    remove URLs, will specify command line

--- Todos.Web/Program.cs
+++ Todos.Web/Program.cs
@@ -19,7 +19,7 @@ namespace Todos.Web
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseUrls("http://localhost:5000", "http://definition.ist:5000")
+//                .UseUrls("http://localhost:5000", "http://definition.ist:5000")
                 .UseStartup<Startup>();
     }
 }
